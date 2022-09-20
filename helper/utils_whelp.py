ed1ec20ebc65e8da2f157d5026fef04bb27bbace
Benjamin VAUDOUR <b.vaudour@luence.fr>
2022-09-20 09:36:39 +0200

    Conversion from python2 to python3

--- helper/utils_whelp.py
+++ helper/utils_whelp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from subprocess import Popen, PIPE
@@ -93,38 +93,37 @@ broken_item_html = '<a href="%(name)s.html">%(name)s</a><br>'
 broken_html = "<h2  style='color:red;margin-left:-35px;margin-top:-20px;'><strong>Broken packages " \
               "[%s]</strong></h2><hr style='margin-left:-15px;margin-top:5px;'><br>"
 
-item_html = u'<li class="portfolio-item2" data-id="id-%(i)s" data-type="%(category)s" data-name' \
-            u'="%(name)s"><div>\
+item_html = '<li class="portfolio-item2" data-id="id-%(i)s" data-type="%(category)s" data-name' \
+            '="%(name)s"><div>\
               <span class="image-block"> <a class="image-zoom" href="%(screenshot)s" rel="prettyPhoto"  \
               title="%(name)s: %(description)s"><img width="170" height="130" src="%(screenshot)s" \
             alt="%(name)s" title="%(name)s" ></a> </span> <div class="home-portfolio-text"> <h2 \
               class="post-title-portfolio"><a href="items/%(' \
-            u'name)s.html?iframe=true&width=800&height=500""\
+            'name)s.html?iframe=true&width=800&height=500""\
               rel="prettyPhoto[iframes]">%(name)s</a></h2> <p class="post-subtitle-portfolio">%(' \
-            u'description)s </p> </div> </div></li>'
+            'description)s </p> </div> </div></li>'
 
 html_item_detail_head = '<h2  style="margin-left:-15px;margin-top:-20px;"><strong>%(name)s </strong>%(pkgver)s-%(pkgrel)s&nbsp;&nbsp;&nbsp;&nbsp;</h2><h4 style="  position: absolute;left:580px;top:0;vertical-align: middle;"><a href="https://github.com/KaOS-Community-Packages/%(name)s"  target="_blank">View on GitHub&nbsp;<img src="../images/github.png" width="22" height="22"></a></h4><hr style="margin-left:-15px;margin-top:5px;"><br>'
 
 private_keys = "&client_id=e43da6309b975672c1eb" \
                "&client_secret=e362ddd7f374c69f2709c2af24b2d7f8e539cce7"
 
-html_item_detail_buttons = u'<div class="container-block"><div class=" text-center"><div class="well">' \
-                           u'<h3>How to install?</h3><hr ><ul   style="margin-left: ' \
-                           u'14px;margin-top: 15px"><strong>KCP helper</strong></br><li class="ui-state-default"><table ' \
-                           u'tyle="width:100%%"> <tr><td width="75%%">Searching or getting the ' \
-                           u'needed files from KaOS Community Packages has been simplified with the ' \
-                           u'addition of the package “kcp”. You can click the button to copy the required' \
-                           u' command kcp and paste it into your console.</td><td ' \
-                           u'width="5%%"></td><td style="vertical-align: mid;" width="20%%"><a class="btn button ' \
-                           u'big" data-clipboard-text="kcp -u && kcp -i %(name)s" title="click to copy to ' \
-                           u'clipboard">Copy command</a></td></tr></table></li></ul><ul ' \
-                           u'style="margin-left: 14px"> <strong>ZIP file</strong><li ' \
-                           u'class="ui-state-default"><table style="width:100%%"><tr><td ' \
-                           u'width="75%%">Click the just downloaded package zip and extract file to your build' \
-                           u' folder. The call to start to build and install the needed dependencies' \
-                           u' is <b>makepkg -si</b>.</td><td width="5%%"></td><td ' \
-                           u'style="vertical-align: mid;" width="20%%"><a target="_blank" ' \
-                           u'href="https://github.com/KaOS-Community-Packages/%(name)s' \
-                           u'/archive/master.zip" class="button big">Download ZIP</a></td>' \
-                           u'</tr></table></li></br></ul></div></div>'
-
+html_item_detail_buttons = '<div class="container-block"><div class=" text-center"><div class="well">' \
+                           '<h3>How to install?</h3><hr ><ul   style="margin-left: ' \
+                           '14px;margin-top: 15px"><strong>KCP helper</strong></br><li class="ui-state-default"><table ' \
+                           'tyle="width:100%%"> <tr><td width="75%%">Searching or getting the ' \
+                           'needed files from KaOS Community Packages has been simplified with the ' \
+                           'addition of the package “kcp”. You can click the button to copy the required' \
+                           ' command kcp and paste it into your console.</td><td ' \
+                           'width="5%%"></td><td style="vertical-align: mid;" width="20%%"><a class="btn button ' \
+                           'big" data-clipboard-text="kcp -u && kcp -i %(name)s" title="click to copy to ' \
+                           'clipboard">Copy command</a></td></tr></table></li></ul><ul ' \
+                           'style="margin-left: 14px"> <strong>ZIP file</strong><li ' \
+                           'class="ui-state-default"><table style="width:100%%"><tr><td ' \
+                           'width="75%%">Click the just downloaded package zip and extract file to your build' \
+                           ' folder. The call to start to build and install the needed dependencies' \
+                           ' is <b>makepkg -si</b>.</td><td width="5%%"></td><td ' \
+                           'style="vertical-align: mid;" width="20%%"><a target="_blank" ' \
+                           'href="https://github.com/KaOS-Community-Packages/%(name)s' \
+                           '/archive/master.zip" class="button big">Download ZIP</a></td>' \
+                           '</tr></table></li></br></ul></div></div>'
